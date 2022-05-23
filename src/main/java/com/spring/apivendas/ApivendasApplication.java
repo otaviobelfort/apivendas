57c253f9dc66153424b814ba2f2e3a2d59341ac4
Igor Pinheiro <igor.serra@ssp.ma.gov.br>
2022-05-23 10:04:42 -0300

    criando projeto

---
+++ src/main/java/com/spring/apivendas/ApivendasApplication.java
@@ -0,0 +1,13 @@
+package com.spring.apivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivendasApplication.class, args);
+	}
+
+}
