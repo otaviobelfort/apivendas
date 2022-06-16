7a2b050af4ae69ca95e7ed7255c55655be12a1cb
otaviobelfort <otaviobelfort@obs>
2022-06-16 15:45:49 -0300

    otimização delete

--- src/main/java/com/spring/apivendas/controller/ClienteController.java
+++ src/main/java/com/spring/apivendas/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.spring.apivendas.controller;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.spring.apivendas.entity.Cliente;
 import com.spring.apivendas.repository.ClientesRepository;
+
+import antlr.collections.List;
 
 @Controller
 @RequestMapping("/clientes")
@@ -28,6 +32,22 @@ public class ClienteController {
 		Optional<Cliente> clientes = clientesRepository.findById(id);
 		if(clientes.isPresent()) {
 			return ResponseEntity.ok(clientes.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping
+	@ResponseBody
+	public java.util.List<Cliente> listaClientes() { //@PathVariable("id") @RequestBody Integer id)
+		return clientesRepository.findAll();
+	}
+	
+	@PostMapping("/salvar")
+	@ResponseBody
+	public ResponseEntity salvarCliente(@RequestBody Cliente cliente) { //@PathVariable("id") @RequestBody Integer id)
+		if(cliente!=null) {
+			return ResponseEntity.ok(clientesRepository.save(cliente));
 		}
 		
 		return ResponseEntity.notFound().build();
